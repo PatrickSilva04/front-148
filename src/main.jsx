faa050045d67dc536b715ef78e3649353607dd0b
Patrick Ferreira da Silva <99125354+PatrickSilva04@users.noreply.github.com>
2025-06-02 16:29:26 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
-
 import './index.css'
 import App from './App.jsx'
 import UsersList from './usersList.jsx'
+import Dashboard from './Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>}/>
+        <Route path='/Dashboard' element={<Dashboard/>}/>
         <Route path='/userslist' element={<UsersList/>}/>
       </Routes>
     </BrowserRouter>
